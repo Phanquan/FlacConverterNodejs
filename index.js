337c0237d4556ae44f2cd07ce5c3cf2adbb1881e
Phanquan <phanquan.2401@gmail.com>
2017-06-06 17:13:57 +0700

    Main class

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-const program = require("caporal");
-const Converter = require('Converter.js')
-const Infomation = require('Infomation.js')
-program
-	.version("1.0.0")
-	.command("Folder", "deploy our command")
-	.help("Convert the whole Folder")
-	.argument("<bitRate>", "bitrate of output file - eg: 128k,320k")
-	.argument("<inputPath>", "input path")
-	.argument("<outputPath>", "output path")
-	.action(function (args) {
-		let testSourceFolder = "E:/Lossless Music/Vocaloid Lossloess/40mP - 幸福指数";
-		let testTargetFolder = "C:/Users/admin/Desktop/Test Converter";
-		let bitrate = args.bitRate;
-		let inputPath = args.inputPath;
-		let outputPath = args.outputPath;
-		console.log("Bitrate: ", bitrate);
-		console.log("Input Path: ", inputPath);
-		console.log("Output Path: ", outputPath);
-	});
+const program = require('caporal')
+const fs = require('fs')
+const path = require('path')
+const Converter = require('./Converter.js').Converter
+const FolderInformation = require('./FolderInfomation.js').FolderInformation
 
-program.parse(process.argv);
+
+
+
+program
+	.version('1.0.0')
+	.command('Folder', 'deploy our command')
+	.argument('<bitRate>', 'bitrate of output files - eg: 128k,320k')
+	.argument('<inputPath>', 'input path')
+	.argument('<outputPath>', 'output path')
+	.action(function (args) {
+		let testSourceFolder = args.inputPath,
+			testTargetFolder = args.outputPath,
+			bitrate = args.bitRate
+
+		let info = new FolderInformation()
+		info.getInputFolderAndFiles(testSourceFolder)
+		info.getOutputFolderAndFiles(testSourceFolder, testTargetFolder)
+
+		let converter = new Converter()
+		converter.createOutputFolder(info.folderData.arrOfOutputFolder, testSourceFolder, testTargetFolder)
+		converter.createOutputFiles(info.fileData.arrOfInputFiles, info.fileData.arrOfOutputFiles)
+		converter.convertDirectory(bitrate, info.fileData.arrOfInputFlacs, info.fileData.arrOfOutputFlacs)
+	})
+	.command('File', 'Convert a single Flac file')
+	.argument('<bitRate', 'bitrate of output file')
+	.argument('<inputPath>', 'input path')
+	.argument('<outputPath>', 'output path')
+	.action(function (args) {
+		let testSourceFolder = args.inputPath,
+			testTargetFolder = args.outputPath,
+			bitrate = args.bitRate
+
+		let converter = new Converter()
+		converter.convertFile(bitrate, testSourceFolder, testTargetFolder)
+	})
+
+program.parse(process.argv)
